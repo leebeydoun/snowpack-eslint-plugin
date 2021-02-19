e5a9235a1b36f9607a9393720a3a70674d3b5b0d
Lee <eldestory@gmail.com>
2021-02-19 07:20:41 +0000

    Fixed typescript config

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
 import type { SnowpackPluginFactory } from 'snowpack'
 
-const plugin: SnowpackPluginFactory = () => ({
-  name: 'snowpack-eslint-plugin',
-})
+// export interface SnowpackEslintPluginOptions extends Object {
+//   input?: Array<string>
+// }
+
+const plugin: SnowpackPluginFactory = () => {
+  // const pOptions = pluginOptions as SnowpackEslintPluginOptions
+
+  return {
+    name: 'snowpack-eslint-plugin',
+    config() {
+      console.log('success')
+    },
+  }
+}
 
 export default plugin
