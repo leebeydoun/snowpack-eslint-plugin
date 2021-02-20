f074ecadcb4a761defe2caf19c1e1a1af09bd647
Lee <eldestory@gmail.com>
2021-02-20 07:51:00 +0000

    Added logic to handling colouring for codeframe and stylish eslint formats

--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,16 @@ import chalk from 'chalk'
 
 const CLEAR_SEQUENCES = ['\x1Bc', '\x1B[2J\x1B[0;0f']
 
-const applyColour = (lintErr: string, formatter: string) => {
-  const lines = lintErr.split('\n')
+const enum SupportedEslintFormatters {
+  CODEFRAME,
+  STYLISH,
+  UNSUPPORTED,
+}
 
-  if (formatter.toLowerCase() === 'codeframe') {
+const applyColour = (lintResultString: string, formatter: SupportedEslintFormatters) => {
+  const lines = lintResultString.split('\n')
+
+  if (formatter === SupportedEslintFormatters.CODEFRAME) {
     const formattedLines = lines.map((line) => {
       if (line.startsWith('error')) return chalk.red(line)
       else if (line.startsWith('warning')) return chalk.yellow(line)
@@ -17,18 +23,17 @@ const applyColour = (lintErr: string, formatter: string) => {
     return formattedLines.join('\n')
   }
 
-  // Matches strings like:
-  //   1:17  warning  'FC' is
-  const warningRegex = /^\s*[0-9]*:[0-9]*\s+warning/g
-  const errorRegex = /^\s*[0-9]*:[0-9]*\s+error/g
+  if (SupportedEslintFormatters.STYLISH) {
+    const formattedLines = lines.map((line) => {
+      if (/^\s*[0-9]*:[0-9]*\s+warning/g.test(line)) return chalk.yellow(line)
+      else if (/^\s*[0-9]*:[0-9]*\s+error/g.test(line)) return chalk.red(line)
+      return line
+    })
 
-  const formattedLines = lines.map((line) => {
-    if (warningRegex.test(line)) return chalk.yellow(line)
-    else if (errorRegex.test(line)) return chalk.red(line)
-    return line
-  })
+    return formattedLines.join('\n')
+  }
 
-  return formattedLines.join('\n')
+  return lintResultString
 }
 
 export interface SnowpackEslintPluginOptions extends Object {
@@ -61,7 +66,8 @@ const plugin: SnowpackPluginFactory = (
     name,
     // @ts-ignore
     async run({ isDev, log }) {
-      const workerPromise = execa.command(isDev ? watchCmd : cmd, {
+      const commandToRun = isDev ? watchCmd : cmd
+      const workerPromise = execa.command(commandToRun, {
         env: npmRunPath.env(),
         extendEnv: true,
         shell: true,
@@ -70,8 +76,18 @@ const plugin: SnowpackPluginFactory = (
       })
       const { stdout, stderr } = workerPromise
 
+      // Stylish by default
+      let formatter = SupportedEslintFormatters.STYLISH
+
+      // If a format flag was specified, we check what value was provided.
+      if (/\s+-f\s+(?!stylish)/.test(commandToRun)) {
+        formatter = /-f\s+codeframe\s*/g.test(commandToRun.toLowerCase())
+          ? SupportedEslintFormatters.CODEFRAME
+          : SupportedEslintFormatters.UNSUPPORTED
+      }
+
       function dataListener(chunk: any) {
-        let stdOutput = applyColour(chunk.toString(), '')
+        let stdOutput = applyColour(chunk.toString(), formatter)
         if (output === 'stream') {
           log('CONSOLE_INFO', { msg: stdOutput })
           return
